00d4fab18b41bc0e3a57ab1ad866d940e1a894d0
Priyank <priyaaank@gmail.com>
2011-02-15 17:36:58 +0530

    Added logic to recommend tweets[incomplete]. Changed data loader logic also.

--- twitter_data_loader.rb
+++ twitter_data_loader.rb
@@ -32,54 +32,59 @@ module Twitter
       puts "*"*100
       people_ids = Person.all.each.map(&:twitter_id).map(&:to_i)
       TweetStream::Client.new(ENV["TWITTER_USERNAME"], ENV["TWITTER_PASSWORD"]).follow(*people_ids) do |orig_status|
-        status = orig_status.clone
-        is_retweet = false
-
         begin
-          status.retweeted_status
-          is_retweet = true
+
+          status = orig_status.clone
+          is_retweet = false
+
+          begin
+            status.retweeted_status
+            is_retweet = true
+          rescue 
+            #do nothing
+          end
+
+          person = Person.find(:first, :conditions => {:twitter_id => status.user.id_str}) ||
+                   Person.create(:screen_name => status.user.screen_name, :twitter_id => status.user.id_str)
+          if is_retweet 
+            original_tweeter = status.retweeted_status.user
+
+            puts "*"*100
+            puts "It's a retweet! The values are:"
+            puts "Status Id: #{status.retweeted_status.id_str}"
+            puts "Status Text: #{status.retweeted_status.text}"
+            puts "Original Tweeter Screenname: #{original_tweeter.screen_name}"
+            puts "Original Tweeter id: #{original_tweeter.id_str}"
+            puts "Person screen name is #{person.screen_name}"
+            puts "Locally stored id is: #{person.twitter_id}"
+            puts "*"*100
+            
+            current_tweet = Tweet.find(:first, :conditions => {:twitter_id => status.retweeted_status.id_str }) || 
+                            Tweet.create(:text       => status.text,
+                                         :twitter_id => status.retweeted_status.id_str)
+            original_tweeter = Person.find(:first, :conditions => {:twitter_id => original_tweeter.id_str}) || 
+                               Person.create(:screen_name => original_tweeter.screen_name,
+                                             :twitter_id  => original_tweeter.id_str)
+            current_tweet.retweeters << person
+            current_tweet.owner << original_tweeter 
+            current_tweet.save!
+          else
+            puts "*"*100
+            puts "It's an original Tweet"
+            puts "Status Id: #{status.id_str}"
+            puts "Status Text: #{status.text}"
+            puts "Tweeter Id: #{status.user.id_str}"
+            puts "Person screen name is #{person.screen_name}"
+            puts "Locally stored id is: #{person.twitter_id}"
+            puts "*"*100
+
+            current_tweet = Tweet.create(:text       => status.text,
+                                         :twitter_id => status.id_str)
+            current_tweet.owner << person
+            current_tweet.save!
+          end
         rescue 
-          #do nothing
-        end
-
-        person = Person.find(:first, :conditions => {:twitter_id => status.user.id_str}) ||
-                 Person.create(:screen_name => status.user.screen_name, :twitter_id => status.user.id_str)
-        if is_retweet 
-          original_tweeter = status.retweeted_status.user
-
-          puts "*"*100
-          puts "It's a retweet! The values are:"
-          puts "Status Id: #{status.retweeted_status.id_str}"
-          puts "Status Text: #{status.retweeted_status.text}"
-          puts "Original Tweeter Screenname: #{original_tweeter.screen_name}"
-          puts "Original Tweeter id: #{original_tweeter.id_str}"
-          puts "Person screen name is #{person.screen_name}"
-          puts "Locally stored id is: #{person.twitter_id}"
-          puts "*"*100
-          
-          current_tweet = Tweet.find(:first, :conditions => {:twitter_id => status.retweeted_status.id_str }) || 
-                          Tweet.create(:text       => status.text,
-                                       :twitter_id => status.retweeted_status.id_str)
-          original_tweeter = Person.find(:first, :conditions => {:twitter_id => original_tweeter.id_str}) || 
-                             Person.create(:screen_name => original_tweeter.screen_name,
-                                           :twitter_id  => original_tweeter.id_str)
-          current_tweet.retweeters << person
-          current_tweet.owner << original_tweeter 
-          current_tweet.save!
-        else
-          puts "*"*100
-          puts "It's an original Tweet"
-          puts "Status Id: #{status.id_str}"
-          puts "Status Text: #{status.text}"
-          puts "Tweeter Id: #{status.user.id_str}"
-          puts "Person screen name is #{person.screen_name}"
-          puts "Locally stored id is: #{person.twitter_id}"
-          puts "*"*100
-
-          current_tweet = Tweet.create(:text       => status.text,
-                                       :twitter_id => status.id_str)
-          current_tweet.owner << person
-          current_tweet.save!
+          puts "Some exception occured, but will continue to next one for now!"
         end
       end
     end
--- twitter_recommendation_engine.rb
+++ twitter_recommendation_engine.rb
@@ -4,21 +4,62 @@ module Twitter
   module Recommendations
     
     def self.generate_master_tweet_list_for(user)
-      puts user.inspect
       tweet_hash_with_empty_weight = user.retweets.each.inject({}) {|hash, tweet| hash[tweet.twitter_id] = 0; hash}
       puts tweet_hash_with_empty_weight
-      Person.all.each.inject({}) {|hash, person| hash[person.screen_name] = tweet_hash_with_empty_weight; hash }
+      master_graph = Person.all.each.inject({}) {|hash, person| hash[person.screen_name] = tweet_hash_with_empty_weight; hash }
+      user.retweets.each do |retweet|
+        Person.all.each do |person|
+          master_graph[person][retweet] = 1 if person.retweets.map(&:twitter_id).include?(retweet.twitter_id)
+        end
+      end
     end
 
     def self.get_cached_master_tweet_list_for(user)
       @tweet_master_set ||= generate_master_tweet_list_for(user)
     end
 
-    def self.calculate_similarity_between(tweet_master_set, user, person)
-      get_cached_master_tweet_list_for(user)
+    def self.calculate_similarity_between(user, person)
+      retweet_graph_for_all_users = get_cached_master_tweet_list_for(user)
+
+      common_retweets_user_and_person = retweet_graph_for_all_users[user].keys & retweet_graph_for_all_users[person].keys
+      total_number_of_common_retweets = common_retweets_user_and_person.count
+
+      #Using Pearson's corelation method to calculate similarity between two datasets of user and a person
+      user_retweet_data_sum = 0
+      person_retweet_data_sum = 0
+      user_person_retweet_data_sum = 0
+      user_tweet_data_square_sum = 0
+      person_retweet_square_data_sum = 0
+
+      common_retweets_user_and_person.each do |retweet|
+        current_rating_for_user = retweet_graph_for_all_users[user][retweet] 
+        current_rating_for_person = retweet_graph_for_all_users[person][retweet] 
+
+        user_retweet_data_sum += current_rating_for_user 
+        person_retweet_data_sum += current_rating_for_person 
+
+        user_person_retweet_data_sum += (current_rating_for_user*current_rating_for_person) 
+
+        user_tweet_data_square_sum += (current_rating_for_user**2)
+        person_tweet_data_square_sum += (current_rating_for_person**2)
+      end
+
+      number = user_person_retweet_data_sum - ((user_retweet_data_sum * person_retweet_data_sum) / total_number_of_common_retweets) 
+      denominator = Math.sqrt((user_tweet_data_square_sum - ((user_retweet_data_sum **2)/total_number_of_common_retweets)) *
+                              (person_retweet_square_data_sum - ((person_retweet_data_sum**2)/total_number_of_common_retweets)))
+      return 0 if denominator == 0
+      number / denominator
     end
 
-    def self.recommend_tweets(tweet_master_set, person)
+    def self.recommend_tweets(tweet_master_set, user)
+      user_similarity_score = {}
+      tweets_user_has_not_retweeted = (Tweet.all.each.map(&:twitter_id) - user.retweets.each.map(&:twitter_id))
+
+      Person.all.each do |person|
+        next if user == person
+        user_similarity_score[person] = calculate_similarity_between(user, person)
+      end
+
 
     end
   end
